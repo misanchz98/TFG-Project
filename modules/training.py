24ebcd4c3dec924f7303cf4c52156d0537953d41
Maria Isabel Sanchez Sanchez <mi.sanchezs.2017@alumnos.urjc.es>
2022-08-21 11:32:25 +0200

    eliminamos dependencias de la herramienta cumulator

--- modules/training.py
+++ modules/training.py
@@ -5,7 +5,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from codecarbon import EmissionsTracker
 from modules.preprocess import split_dataset, get_features, get_output
-from cumulator import base
 
 
 # CODECARBON
@@ -19,13 +18,13 @@ def train_LR_codecarbon(df, train_size=0.25):
     print(y)
 
     # Step 3: train LogisticRegression model
-    # lg_pipeline = Pipeline([("scaler", StandardScaler()), ("logistic_regression", LogisticRegression())])
-    # tracker = EmissionsTracker()
+    lg_pipeline = Pipeline([("scaler", StandardScaler()), ("logistic_regression", LogisticRegression())])
+    tracker = EmissionsTracker()
 
-    # tracker.start()
-    # lg_pipeline.fit(X, y)
-    # emissions: float = tracker.stop()
-    # print(f"Emissions: {emissions} kg")
+    tracker.start()
+    lg_pipeline.fit(train_features, y)
+    emissions: float = tracker.stop()
+    print(f"Emissions: {emissions} kg")
 
 
 def train_RF_codecarbon(train_features, train_output, n_estimators=100, max_leaf_nodes=16, n_jobs=-1):
@@ -46,28 +45,3 @@ def train_SVC_codecarbon(train_features, train_output):
     svm_pipeline.fit(train_features, train_output)
     emissions: float = tracker.stop()
     print(f"Emissions: {emissions} kg")
-
-
-# CUMULATOR
-def train_LR_cumulator(train_features, train_output):
-    lg_pipeline = Pipeline([("scaler", StandardScaler()), ("logistic_regression", LogisticRegression())])
-    cumulator = base.Cumulator()
-    cumulator.run(lg_pipeline.fit, X=train_features, y=train_output)
-    print(cumulator.total_carbon_footprint())
-    cumulator.display_carbon_footprint()
-
-
-def train_RF_cumulator(train_features, train_output, n_estimators=100, max_leaf_nodes=16, n_jobs=-1):
-    rnd_clf = RandomForestClassifier(n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, n_jobs=n_jobs)
-    cumulator = base.Cumulator()
-    cumulator.run(rnd_clf.fit, X=train_features, y=train_output)
-    print(cumulator.total_carbon_footprint())
-    cumulator.display_carbon_footprint()
-
-
-def train_SVC_cumulator(train_features, train_output):
-    svm_pipeline = Pipeline([("scaler", StandardScaler()), ("linear_svc", LinearSVC(C=1, loss="hinge"))])
-    cumulator = base.Cumulator()
-    cumulator.run(svm_pipeline.fit, X=train_features, y=train_output)
-    print(cumulator.total_carbon_footprint())
-    cumulator.display_carbon_footprint()
